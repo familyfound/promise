fff7e1ed3cdd20d36f99d30c702cc0de844373d8
Jared Forsyth <jared@jaredforsyth.com>
2013-06-25 00:35:52 -0600

    initial

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# promise
+
+  Promised data
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install familyfound/promise
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,22 @@
+var request = require('superagent');
+
+function promise(getter, next) {
+  var item = null, waiting = [];
+  var superagent = getter instanceof request.Request;
+  if (superagent) getter = getter.end;
+  getter(function (err, res) {
+    if (err) return next(err);
+    if (superagent) { res = res.body; }
+    item = res;
+    waiting.forEach(function (fn) {
+      fn(item);
+    });
+    next(null, item);
+  });
+  return function (fn) {
+    if (item !== null) return fn(item);
+    waiting.push(fn);
+  };
+}
+
+module.exports = promise;
